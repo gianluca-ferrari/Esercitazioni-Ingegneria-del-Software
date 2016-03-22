d923921f7c0e16ff291735ac77fcd41852f59d26
xxoppyxx <oppy.luca@gmail.com>
2016-03-22 15:46:50 +0100

    stupent with methods

--- Esercitazioni Java/src/ex03/student/Student.java
+++ Esercitazioni Java/src/ex03/student/Student.java
@@ -1,56 +1,33 @@
-package it.polimi.deib.se.ex03.student;
+package ex03.student;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
-/*
- * Rappresentiamo uno studente come una persona cui e' associata
- * una lista di esami.
- */
 public class Student extends Person {
-	private static final int GRADUATION_CREDITS = 180;
-	private final List<Grade> grades = new ArrayList<Grade>();
-	
-	public List<Grade> getGrades(){
-		return new ArrayList<Grade>(grades);
-	}
 
+	private final List<Grade> grades
+	
 	public Student(String name, Calendar birthday) {
 		super(name, birthday);
 	}
-
-	// Aggiunge una valutazione alla carriera dello studente.
-	public void addGrade(Grade grade) {
-		grades.add(grade);
+	
+	public void addGrade(Grade grade){
+		
 	}
-
-	// Controlla se lo studente ha abbastanza crediti per potersi laureare.
-	public boolean canGraduate() {
-		return totalCredits() >= GRADUATION_CREDITS;
+	
+	public boolean canGraduate(){
+		
 	}
-
-	// Calcola la media pesata.
-	public double getWeightedGradeAverage() {
-		int sumOfWeightedPoints = 0;
-		for (Grade grade : grades) {
-			sumOfWeightedPoints += grade.getCredits() * grade.getPoints();
-		}
-		return ((double)sumOfWeightedPoints) / totalCredits();
+	
+	public double getWeightedGradeAverage(){
+		
 	}
-
-	// Calcola il numero di crediti sostenuti dallo studente.
-	private int totalCredits() {
-		int totalCredits = 0;
-		for (Grade grade : grades) {
-			totalCredits += grade.getCredits();
-		}
-		return totalCredits;
+	
+	private int totalCredits(){
+		
 	}
-
+	
 	@Override
-	public String toString() {
-		return super.toString() + " and I am a student";
+	public String toString(){
+		
 	}
-
 }
